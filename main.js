2fe1d735e5ed7b2b8a0bab172bae4e7858316347
szdc <jack.willis-craig@hotmail.com>
2014-11-17 19:10:42 +1100

    The statement is now inserted below any jslint/jshint/global comments

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-'use strict';
-
+/*jslint node: true */
 /*global define, $, brackets, window, console */
 
-define(function (require, exports, module) {
-  //'use strict';
+'use strict';
 
+define(function (require, exports, module) {
   var CONSOLE_PREFIX = '[use-strict-js]: ';
 
   var CommandManager  = brackets.getModule('command/CommandManager'),
@@ -13,10 +12,29 @@ define(function (require, exports, module) {
 
   function processJSDocument(document) {
     var text = document.getText();
+    
+    // Is this file already strict?
     var isStrict = /^\s*?["']use strict["']/m.test(text);
     console.log(CONSOLE_PREFIX + 'Is Strict: ' + isStrict);
+
     if (!isStrict) {
-      document.replaceRange("'use strict';\n\n", { line: 0, ch: 0 });
+      // NodeJS files have the statement at the top of the file
+      var isNodeJS = /\/\*js[lh]int.+?node:\s?true/m.test(text);
+      console.log(CONSOLE_PREFIX + 'Is Node.JS file: ' + isNodeJS);
+      
+      // Add the statement below any jslint/jshint/comments
+      // The first line that isn't 
+      var insertionLineIndex = 0;
+      var lines = text.split('\n');
+      lines.some(function (line, index) {
+        if (line.search(/\/\*/) === -1) {
+          insertionLineIndex = index;
+          console.log(CONSOLE_PREFIX + 'Insert at line: ' + insertionLineIndex);
+          return true;
+        }
+      });
+      
+      document.replaceRange("\n'use strict';\n", { line: insertionLineIndex, ch: 0 });
       CommandManager.execute(Commands.FILE_SAVE, { doc: document });
     }
   }
